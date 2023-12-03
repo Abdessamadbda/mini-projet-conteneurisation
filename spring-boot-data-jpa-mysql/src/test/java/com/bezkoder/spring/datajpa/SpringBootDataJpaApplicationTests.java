62f6d6b24f305f9e62e05a3d5a117caac543d583
Abdessamadbda <abdessamadelbada02@gmail.com>
2023-12-03 18:55:21 +0000

    first commit

---
+++ spring-boot-data-jpa-mysql/src/test/java/com/bezkoder/spring/datajpa/SpringBootDataJpaApplicationTests.java
@@ -0,0 +1,9 @@
+package com.bezkoder.spring.datajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDataJpaApplicationTests {
+
+}
